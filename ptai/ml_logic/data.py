cdd9d2e32c455f350f1e5819d01c9b714b713377
Mischa Dhar <mischadhar94@gmail.com>
2023-12-01 10:32:58 +0000

    finishing cropping images function

--- ptai/ml_logic/data.py
+++ ptai/ml_logic/data.py
@@ -1,39 +1,43 @@
 import os
 import json
+from tqdm import tqdm
 
 from ptai.data_preproc import crop_images
 
 def generate_cropped_images(path_to_raw_data):
 
-    with open(os.path.join(path_to_raw_data, "annotations_cleaned_IoU.json"), 'r') as file:
+    with open(os.path.join(path_to_raw_data, "../annotations_cleaned_IoU.json"), 'r') as file:
         data = json.load(file)
 
     data_no_overlap = [annotation for annotation in data if annotation["iou_cat"] == False]
 
-    for annotation in data_no_overlap:
+    for annotation in tqdm(data_no_overlap):
         image_id = annotation["image_id"]
         bbox = annotation["bbox"]
         iou = sum(annotation["iou"])
         pose_variation = annotation["pose_variation"]
 
-        image_id_len = len(str(image_id))
-        zeros = "0" * (8 - image_id_len)
+        if pose_variation in ['lunge_left', 'lunge_right', 'squats', 'pushups']:
 
-        file_name = f"{zeros}{image_id}.rgb.png"
+            image_id_len = len(str(image_id))
+            zeros = "0" * (8 - image_id_len)
 
-        cropped_image = crop_images.crop_image_to_bounding_box(file_name, os.path.join(path_to_raw_data,"00"), bbox)
+            file_name = f"{zeros}{image_id}.rgb.png"
 
-        pose_subdirs = os.listdir(os.path.join(path_to_raw_data, "cropped_data"))
+            cropped_image = crop_images.crop_image_to_bounding_box(file_name, os.path.join(path_to_raw_data), bbox)
 
-        if pose_variation not in pose_subdirs:
-            os.mkdir(os.path.join(path_to_raw_data, "cropped_data", pose_variation))
+            pose_subdirs = os.listdir(os.path.join(path_to_raw_data, "../cropped_data"))
 
-        crop_images.save_cropped_image(cropped_image
-                                    , os.path.join(path_to_raw_data, "cropped_data", pose_variation)
-                                    , file_name + "_" + pose_variation + "_" + str(round(iou,3)) + "_cropped.png")
+            if pose_variation not in pose_subdirs:
+                os.mkdir(os.path.join(path_to_raw_data, "../cropped_data", pose_variation))
+
+            crop_images.save_cropped_image(cropped_image
+                                        , os.path.join(path_to_raw_data, "../cropped_data", pose_variation)
+                                        , file_name + "_" + pose_variation + "_" + str(round(iou,3)) + "_cropped.png")
 
 if __name__ == "__main__":
 
     path_to_raw_data = "raw_data"
+    unprocessed_data_folder = os.path.join(path_to_raw_data,"unprocessed_data")
 
-    generate_cropped_images(path_to_raw_data)
+    generate_cropped_images(unprocessed_data_folder)
