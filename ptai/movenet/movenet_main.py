33c6d082a284448d8e477611e3f85d664d05309d
Mischa Dhar <mischadhar94@gmail.com>
2023-12-03 19:06:12 +0000

    created a folder to store all the functions necessary to run movenet on a still image (loaded locally)

---
+++ ptai/movenet/movenet_main.py
@@ -0,0 +1,48 @@
+# IMPORTS
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ptai.movenet.movenet_model import load_model_from_tfhub, run_movenet_inference
+from ptai.movenet.movenet_data import load_image_data, load_image_for_skeleton
+from ptai.movenet.movenet_plot_img import draw_prediction_on_image
+
+def load_model_and_run_inference(image_path):
+
+    """This function:
+    1) Loads the model from tfhub (default is single-pose lightning)
+    2) Loads the image to run inference on either locally or from cloud
+    (to be coded) - default is LOCAL
+    3) Runs inference on the image and returns the keypoints of the skeleton"""
+
+    model = load_model_from_tfhub()
+
+    image = load_image_data(image_path)
+
+    keypoints_with_scores = run_movenet_inference(model, image)
+
+    return keypoints_with_scores
+
+def plot_skeleton_on_image(image_path, keypoints_with_scores):
+
+    display_image = load_image_for_skeleton(image_path)
+
+    output_overlay = draw_prediction_on_image(
+        np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+
+    plt.figure(figsize=(5, 5))
+    plt.imshow(output_overlay)
+    _ = plt.axis('off')
+    plt.show()
+
+# 4) LOAD IMAGE TO PLOT POINTS ON (CAN THIS BE COMBINED WITH STEP 2?)
+
+# 5) PLOT IMAGE WITH SKELETON
+
+if __name__ == "__main__":
+
+    image_path = input("Please enter the path to the image:")
+
+    keypoints_with_scores = load_model_and_run_inference(image_path=image_path)
+
+    plot_skeleton_on_image(image_path, keypoints_with_scores)
---
+++ ptai/movenet/movenet_plot_img.py
@@ -0,0 +1,91 @@
+import cv2
+
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import matplotlib.patches as patches
+
+import numpy as np
+
+from ptai.movenet.movenet_helper import _keypoints_and_edges_for_display
+
+def draw_prediction_on_image(
+    image, keypoints_with_scores, crop_region=None, close_figure=False,
+    output_image_height=None):
+  """Draws the keypoint predictions on image.
+
+  Args:
+    image: A numpy array with shape [height, width, channel] representing the
+      pixel values of the input image.
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    crop_region: A dictionary that defines the coordinates of the bounding box
+      of the crop region in normalized coordinates (see the init_crop_region
+      function below for more detail). If provided, this function will also
+      draw the bounding box on the image.
+    output_image_height: An integer indicating the height of the output image.
+      Note that the image aspect ratio will be the same as the input image.
+
+  Returns:
+    A numpy array with shape [out_height, out_width, channel] representing the
+    image overlaid with keypoint predictions.
+  """
+
+  height, width, channel = image.shape
+
+  print(height, width, channel)
+
+  aspect_ratio = float(width) / height
+
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  # To remove the huge white borders
+  fig.tight_layout(pad=0)
+  ax.margins(0)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.axis('off')
+
+  im = ax.imshow(image)
+
+  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
+  ax.add_collection(line_segments)
+  # Turn off tick labels
+  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
+
+  (keypoint_locs, keypoint_edges,
+   edge_colors) = _keypoints_and_edges_for_display(
+       keypoints_with_scores, height, width)
+
+  line_segments.set_segments(keypoint_edges)
+  line_segments.set_color(edge_colors)
+  if keypoint_edges.shape[0]:
+    line_segments.set_segments(keypoint_edges)
+    line_segments.set_color(edge_colors)
+  if keypoint_locs.shape[0]:
+    scat.set_offsets(keypoint_locs)
+
+  if crop_region is not None:
+    xmin = max(crop_region['x_min'] * width, 0.0)
+    ymin = max(crop_region['y_min'] * height, 0.0)
+    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
+    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
+    rect = patches.Rectangle(
+        (xmin,ymin),rec_width,rec_height,
+        linewidth=1,edgecolor='b',facecolor='none')
+    ax.add_patch(rect)
+
+  fig.canvas.draw()
+
+  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+
+  image_from_plot = image_from_plot.reshape(2400,2400,3)
+
+#   image_from_plot = image_from_plot.reshape(
+    #   fig.canvas.get_width_height()[::-1] + (3,))
+
+  plt.close(fig)
+  if output_image_height is not None:
+    output_image_width = int(output_image_height / height * width)
+    image_from_plot = cv2.resize(
+        image_from_plot, dsize=(output_image_width, output_image_height),
+         interpolation=cv2.INTER_CUBIC)
+  return image_from_plot
