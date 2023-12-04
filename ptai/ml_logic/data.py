a79182d1f91e41489e6f1682e9e4ab4e3ec20510
Mischa Dhar <mischadhar94@gmail.com>
2023-12-04 17:48:13 +0000

    updated data to generate keypoints for each cropped image

--- ptai/ml_logic/data.py
+++ ptai/ml_logic/data.py
@@ -4,11 +4,15 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import json
 
 # Needed for the model filtering
 from tensorflow.keras.utils import image_dataset_from_directory
 
 from ptai.data_preproc import crop_images
+
+from ptai.movenet.movenet_data import load_image_data
+from ptai.movenet.movenet_model import load_model_from_tfhub, run_movenet_inference
 
 def generate_cropped_images(path_to_raw_data):
 
@@ -40,6 +44,35 @@ def generate_cropped_images(path_to_raw_data):
             crop_images.save_cropped_image(cropped_image
                                         , os.path.join(path_to_raw_data, "../cropped_data", pose_variation)
                                         , file_name.removesuffix(".rgb.png") + "_" + pose_variation + "_" + str(round(iou,3)) + "_cropped.png")
+
+def generate_keypoints_for_xgboost(path_to_cropped_images):
+
+    keypoints_list = []
+    pose_list = []
+
+    model = load_model_from_tfhub()
+
+    pose_folders = os.listdir(path_to_cropped_images)
+    pose_folders.remove(".DS_Store")
+
+    for folder in pose_folders:
+
+        folder_path = os.path.join(path_to_cropped_images,folder)
+
+        images = os.listdir(folder_path)
+
+        for image in tqdm(images):
+
+            img_path = os.path.join(os.path.join(path_to_cropped_images,folder),image)
+
+            img_array = load_image_data(img_path)
+
+            keypoints = run_movenet_inference(model, img_array)
+
+            keypoints_list.append(keypoints)
+            pose_list.append(folder)
+
+    return keypoints_list, pose_list
 
 #params
 batch_size = 32
@@ -79,7 +112,28 @@ def validation_dataset_create():
 
 if __name__ == "__main__":
 
-    path_to_raw_data = "raw_data"
-    unprocessed_data_folder = os.path.join(path_to_raw_data,"unprocessed_data")
+    # path_to_raw_data = "raw_data"
+    # unprocessed_data_folder = os.path.join(path_to_raw_data,"unprocessed_data")
 
-    generate_cropped_images(unprocessed_data_folder)
+    # generate_cropped_images(unprocessed_data_folder)
+
+    current_wd = os.getcwd()
+
+    path_to_cropped_images = os.path.join(current_wd,"raw_data/cropped_data")
+
+    X, y = generate_keypoints_for_xgboost(path_to_cropped_images)
+
+    X_filename = os.path.join(current_wd,'raw_data/X_list.json')
+    y_filename = os.path.join(current_wd,'raw_data/y_list.json')
+
+    with open(X_filename, 'w') as file:
+        # Serialize and save the list
+        json.dump(X, file)
+
+    print("Saved X file")
+
+    with open(y_filename, 'w') as file:
+        # Serialize and save the list
+        json.dump(y, file)
+
+    print("Saved y file")
